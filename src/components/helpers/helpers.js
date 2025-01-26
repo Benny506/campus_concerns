75932ba85f04bc9f52d1fa8b2117ff7da3ef799a
Benjamin Ufeh <olomufeh@gmail.com>
2025-01-26 18:29:06 +0100

    updating helpers.js

--- src/components/helpers/helpers.js
+++ src/components/helpers/helpers.js
@@ -1,3 +1,7 @@
 export default {
-    BaseUrl: 'http://localhost:3005/api/v1/'
+    //testing
+    // BaseUrl: 'http://localhost:3005/api/v1/'
+
+    //production
+    BaseUrl: 'https://campus-concerns-api.onrender.com/api/v1/'
 }
